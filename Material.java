5479919c932a4bb679bd58cdadb817b71334dd4b
kevinagayby <kevinagayby5@gmail.com>
2025-05-25 16:46:25 +0300

    commit

---
+++ Material.java
@@ -0,0 +1,11 @@
+public class Material {
+    private String name;
+
+    public Material(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
