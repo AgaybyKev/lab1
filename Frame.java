77aa466a53d1ff8e1aa9c75eb1e41a9adf06b3da
kevinagayby <kevinagayby5@gmail.com>
2025-05-25 16:44:08 +0300

    commit

---
+++ Frame.java
@@ -0,0 +1,13 @@
+class Frame {
+
+    private Material material;
+
+
+    public Frame(Material material) {
+        this.material = material;
+    }
+
+    public Material getMaterial() {
+        return material;
+    }
+}
