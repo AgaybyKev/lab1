a10a24ffeb9b5832db23e048c607f40eb5c1185e
kevinagayby <kevinagayby5@gmail.com>
2025-05-28 21:22:58 +0300

    commit

---
+++ Docs/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"<Unnamed>","l":"Main"},{"p":"<Unnamed>","l":"Material"},{"p":"<Unnamed>","l":"Motorcycle"},{"p":"<Unnamed>","l":"Wheel"}];updateSearchResults();
